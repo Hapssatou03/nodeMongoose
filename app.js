2baecda3b41b21c69169aff427fb420a02317b1e
hapssatou Sy <hapssatousy01@gmail.com>
2024-11-04 16:57:41 +0100

    createdFunction

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const db = require('./database/setup'); // Do not edit/remove this line
-
+const db = require("./database/setup"); // Do not edit/remove this line
 
 // Create todo with name and priority (done = false by default)
-function createTodo(name, priority) {}
+function createTodo(name, priority) {
+  const newTodo = new db({
+    name: name,
+    priority: priority,
+    done: false,
+  });
+  newTodo.save().then(() => {
+    db.find().then((data) => {
+      console.log(data);
+    });
+  });
+}
 
+//createTodo("listen", 5);
 
 // Complete one todo by name (update property done = true)
 function completeTodo(name) {}
 
-
 // Delete one todo by name
 function deleteTodo(name) {}
-
 
 module.exports = { createTodo, completeTodo, deleteTodo }; // Do not edit/remove this line
